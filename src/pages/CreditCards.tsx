cfd33ccc7dd6dfac014c1a17332226b4a95ef122
Narmina-777 <narminahuseynova1999@gmail.com>
2025-07-27 01:49:29 +0400

    Add cards section

--- src/pages/CreditCards.tsx
+++ src/pages/CreditCards.tsx
@@ -1,5 +1,309 @@
+import styles from "../styles/CreditCards.module.css";
 export default function CreditCards() {
   return (
-    <div>CreditCards</div>
-  )
+    <main className={styles.mainContent}>
+      <section className={styles.cardsSection}>
+        <div className={styles.sectionTitle}>My Cards</div>
+        <div className={styles.cardsContainer}>
+          {/* CARD 1 */}
+          <div className={styles.cardWrapper}>
+            <div className={`${styles.card} ${styles.cardBlue}`}>
+              <div className={styles.cardHeader}>
+                <div className={styles.balanceInfo}>
+                  <p className={styles.balanceLabel}>Balance</p>
+                  <p className={styles.balanceValue}>$5,756</p>
+                </div>
+                <img
+                  className={styles.chipIcon}
+                  src="src/assets/dashboard/Chip_Card.png"
+                  alt="pic-logo"
+                />
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardHolder}>
+                  <p className={styles.holderLabel}>CARD HOLDER</p>
+                  <p className={styles.holderName}>Eddy Cusuma</p>
+                </div>
+                <div className={styles.cardValid}>
+                  <p className={styles.validLabel}>VALID THRU</p>
+                  <p className={styles.validDate}>12/22</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.cardFooter}>
+              <p className={styles.cardNumber}>3778 **** **** 1234</p>
+              <img
+                className={styles.bankLogo}
+                src="src/assets/dashboard/Group 17.png"
+                alt="logo-mini"
+              />
+            </div>
+          </div>
+
+          {/* CARD 2 */}
+          <div className={styles.cardWrapper}>
+            <div className={`${styles.card} ${styles.cardDarkBlue}`}>
+              <div className={styles.cardHeader}>
+                <div className={styles.balanceInfo}>
+                  <p className={styles.balanceLabelWhite}>Balance</p>
+                  <p className={styles.balanceValueWhite}>$5,756</p>
+                </div>
+                <img
+                  className={styles.chipIcon}
+                  src="src/assets/dashboard/Chip_Card.png"
+                  alt="pic-logo"
+                />
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardHolder}>
+                  <p className={styles.holderLabelWhite}>CARD HOLDER</p>
+                  <p className={styles.holderNameWhite}>Eddy Cusuma</p>
+                </div>
+                <div className={styles.cardValid}>
+                  <p className={styles.validLabelWhite}>VALID THRU</p>
+                  <p className={styles.validDateWhite}>12/22</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.cardFooterWhite}>
+              <p className={styles.cardNumberWhite}>3778 **** **** 1234</p>
+              <img
+                className={styles.bankLogo}
+                src="src/assets/dashboard/Group 17 (1).png"
+                alt="logo-mini"
+              />
+            </div>
+          </div>
+
+          {/* CARD 3 */}
+          <div className={styles.cardWrapper}>
+            <div className={`${styles.card} ${styles.cardWhite}`}>
+              <div className={styles.cardHeader}>
+                <div className={styles.balanceInfo}>
+                  <p className={styles.balanceLabel}>Balance</p>
+                  <p className={styles.balanceValue}>$5,756</p>
+                </div>
+                <img
+                  className={styles.chipIcon}
+                  src="src/assets/dashboard/Chip_Card (1).png"
+                  alt="pic-logo"
+                />
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.cardHolder}>
+                  <p className={styles.holderLabel}>CARD HOLDER</p>
+                  <p className={styles.holderName}>Eddy Cusuma</p>
+                </div>
+                <div className={styles.cardValid}>
+                  <p className={styles.validLabel}>VALID THRU</p>
+                  <p className={styles.validDate}>12/22</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.cardFooter}>
+              <p className={styles.cardNumber}>3778 **** **** 1234</p>
+              <img
+                className={styles.bankLogo}
+                src="src/assets/dashboard/Group 17.png"
+                alt="logo-mini"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.statisticsSection}>
+        <div className={styles.sectionStats}>
+          <h3 className={styles.sectionSubtitle}>Card Expense Statistics</h3>
+          <div className={styles.statsChart}>
+            <img src="src/assets/creditCards/Group 351.png" alt="pic" />
+          </div>
+        </div>
+
+        <div className={styles.cardListSection}>
+          <h3 className={styles.sectionSubtitle}>Card List</h3>
+
+          <div className={styles.cardItem}>
+            <img src="src/assets/creditCards/credit-card 1.png" alt="pic" />
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Type</h4>
+              <p className={styles.cardItemValue}>Secondary</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Bank</h4>
+              <p className={styles.cardItemValue}>DBL Bank</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Number</h4>
+              <p className={styles.cardItemValue}>**** **** 5600</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Name on Card</h4>
+              <p className={styles.cardItemValue}>William</p>
+            </div>
+            <button className={styles.cardItemButton}>View Details</button>
+          </div>
+
+          <div className={styles.cardItem}>
+            <img src="src/assets/creditCards/Group.png" alt="pic" />
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Type</h4>
+              <p className={styles.cardItemValue}>Secondary</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Bank</h4>
+              <p className={styles.cardItemValue}>BRC Bank</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Number</h4>
+              <p className={styles.cardItemValue}>**** **** 4300</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Name on Card</h4>
+              <p className={styles.cardItemValue}>Michel</p>
+            </div>
+            <button className={styles.cardItemButton}>View Details</button>
+          </div>
+
+          <div className={styles.cardItem}>
+            <img src="src/assets/creditCards/credit-card 1-2.png" alt="pic" />
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Type</h4>
+              <p className={styles.cardItemValue}>Secondary</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Bank</h4>
+              <p className={styles.cardItemValue}>ABM Bank</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Card Number</h4>
+              <p className={styles.cardItemValue}>**** **** 7560</p>
+            </div>
+            <div>
+              <h4 className={styles.cardItemLabel}>Name on Card</h4>
+              <p className={styles.cardItemValue}>Edward</p>
+            </div>
+            <button className={styles.cardItemButton}>View Details</button>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.cardManagementSection}>
+        <div className={styles.addCardSection}>
+          <h3 className={styles.sectionSubtitle}>Add New Card</h3>
+          <div>
+            <p className={styles.description}>
+              Credit Card generally means a plastic card issued by Scheduled
+              Commercial Banks assigned to a Cardholder, with a credit limit...
+            </p>
+
+            <div className={styles.formContainer}>
+              <div className={styles.row}>
+                <div className={styles.inputGroup}>
+                  <label htmlFor="card-type">Card Type</label>
+                  <input
+                    type="text"
+                    id="card-type"
+                    className={styles.inputField}
+                    placeholder="Classic"
+                  />
+                </div>
+                <div className={styles.inputGroup}>
+                  <label htmlFor="name-on-card">Name On Card</label>
+                  <input
+                    type="text"
+                    id="name-on-card"
+                    className={styles.inputField}
+                    placeholder="My Cards"
+                  />
+                </div>
+              </div>
+
+              {/* Второй ряд: Card Number + Expiration Date */}
+              <div className={styles.row}>
+                <div className={styles.inputGroup}>
+                  <label htmlFor="card-number">Card Number</label>
+                  <input
+                    type="text"
+                    id="card-number"
+                    className={styles.inputField}
+                    placeholder="**** **** **** ****"
+                  />
+                </div>
+                <div className={styles.inputGroup}>
+                  <label htmlFor="expiration-date">Expiration Date</label>
+                  <select
+                    id="expiration-date"
+                    className={`${styles.inputField} ${styles.selectField}`}
+                    defaultValue="25 January 2025"
+                  >
+                    <option>25 January 2025</option>
+                    <option>25 January 2026</option>
+                    <option>25 January 2027</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <button className={styles.addCardButton}>Add Card</button>
+          </div>
+        </div>
+
+        <div className={styles.cardSettings}>
+          <h3 className={styles.sectionSubtitle}>Card Setting</h3>
+          <div className={styles.settingOptions}>
+            <div className={styles.settingItem}>
+              <img src="src/assets/creditCards/Group 165.png" alt="pic" />
+              <div>
+                <h4 className={styles.settingTitle}>Block Card</h4>
+                <p className={styles.settingDescription}>
+                  Instantly block your card
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.settingItem}>
+              <img src="src/assets/creditCards/Group 166.png" alt="pic" />
+              <div>
+                <h4 className={styles.settingTitle}>Change Pin Code</h4>
+                <p className={styles.settingDescription}>
+                  Choose another pin code
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.settingItem}>
+              <img src="src/assets/creditCards/Group 167.png" alt="pic" />
+              <div>
+                <h4 className={styles.settingTitle}>Add to Google Pay</h4>
+                <p className={styles.settingDescription}>
+                  Withdraw without any card
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.settingItem}>
+              <img src="src/assets/creditCards/Group 168.png" alt="pic" />
+              <div>
+                <h4 className={styles.settingTitle}>Add to Apple Pay</h4>
+                <p className={styles.settingDescription}>
+                  Withdraw without any card
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.settingItem}>
+              <img src="src/assets/creditCards/Group 168.png" alt="pic" />
+              <div>
+                <h4 className={styles.settingTitle}>Add to Apple Store</h4>
+                <p className={styles.settingDescription}>
+                  Withdraw without any card
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
--- src/pages/Investments.tsx
+++ src/pages/Investments.tsx
@@ -1,3 +1,186 @@
+import styles from "../styles/Investments.module.css";
+
 export default function Investments() {
-  return <div>Investments</div>;
+  return (
+    <main className={styles.main}>
+      <section className={styles.performanceSection}>
+        <div className={styles.statsCards}>
+          <div className={styles.statCard}>
+            <img
+              className={styles.statImage}
+              src="src/assets/investments/Group 303.png"
+              alt="Total Invested"
+            />
+            <div className={styles.statText}>
+              <p className={styles.statLabel}>Total Invested Amount</p>
+              <h4 className={styles.statValue}>$150,000</h4>
+            </div>
+          </div>
+
+          <div className={styles.statCard}>
+            <img
+              className={styles.statImage}
+              src="src/assets/investments/Group 305.png"
+              alt="Investments Count"
+            />
+            <div className={styles.statText}>
+              <p className={styles.statLabel}>Number of Investments</p>
+              <h4 className={styles.statValue}>1,250</h4>
+            </div>
+          </div>
+
+          <div className={styles.statCard}>
+            <img
+              className={styles.statImage}
+              src="src/assets/investments/Group 307.png"
+              alt="Return Rate"
+            />
+            <div className={styles.statText}>
+              <p className={styles.statLabel}>Rate of Return</p>
+              <h4 className={styles.statValue}>+5.80%</h4>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.chartsSection}>
+        <div className={styles.chartBlock}>
+          <h3 className={styles.chartTitle}>Yearly Total Investment</h3>
+          <div className={styles.chartImageWrapper}>
+            <img
+              className={styles.chartImage}
+              src="src/assets/investments/Group 232.png"
+              alt="Yearly Chart"
+            />
+          </div>
+        </div>
+
+        <div className={styles.chartBlock}>
+          <h3 className={styles.chartTitle}>Monthly Revenue</h3>
+          <div className={styles.chartImageWrapper}>
+            <img
+              className={styles.chartImage}
+              src="src/assets/investments/Group 254.png"
+              alt="Monthly Chart"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.investmentsSection}>
+        <div className={styles.myInvestments}>
+          <h3 className={styles.sectionTitle}>My Investment</h3>
+          <div className={styles.investmentCards}>
+            <div className={styles.investmentCard}>
+              <img
+                className={styles.investmentImage}
+                src="src/assets/investments/Group 245.png"
+                alt="Apple"
+              />
+              <div className={styles.investmentCompany}>
+                <h4 className={styles.companyName}>Apple Store</h4>
+                <p className={styles.companySector}>E-commerce, Marketplace</p>
+              </div>
+              <div className={styles.investmentAmount}>
+                <h4 className={styles.amountValue}>$54,000</h4>
+                <p className={styles.amountLabel}>Investment Value</p>
+              </div>
+              <div className={styles.investmentReturn}>
+                <h4 className={styles.returnValue}>+16%</h4>
+                <p className={styles.returnLabel}>Return Value</p>
+              </div>
+            </div>
+
+            <div className={styles.investmentCard}>
+              <img
+                className={styles.investmentImage}
+                src="src/assets/investments/Group 875.png"
+                alt="Samsung"
+              />
+              <div className={styles.investmentCompany}>
+                <h4 className={styles.companyName}>Samsung Mobile</h4>
+                <p className={styles.companySector}>E-commerce, Marketplace</p>
+              </div>
+              <div className={styles.investmentAmount}>
+                <h4 className={styles.amountValue}>$25,300</h4>
+                <p className={styles.amountLabel}>Investment Value</p>
+              </div>
+              <div className={styles.investmentReturn}>
+                <h4 className={styles.returnValue}>-4%</h4>
+                <p className={styles.returnLabel}>Return Value</p>
+              </div>
+            </div>
+
+            <div className={styles.investmentCard}>
+              <img
+                className={styles.investmentImage}
+                src="src/assets/investments/Group 876.png"
+                alt="Tesla"
+              />
+              <div className={styles.investmentCompany}>
+                <h4 className={styles.companyName}>Tesla Motors</h4>
+                <p className={styles.companySector}>Electric Vehicles</p>
+              </div>
+              <div className={styles.investmentAmount}>
+                <h4 className={styles.amountValue}>$8,200</h4>
+                <p className={styles.amountLabel}>Investment Value</p>
+              </div>
+              <div className={styles.investmentReturn}>
+                <h4 className={styles.returnValue}>+25%</h4>
+                <p className={styles.returnLabel}>Return Value</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.trendingStocks}>
+          <h3 className={styles.sectionTitle}>Trending Stock</h3>
+          <div className={styles.stockTableWrapper}>
+            <table className={styles.stockTable}>
+              <thead className={styles.tableHead}>
+                <tr className={styles.tableRow}>
+                  <th className={styles.tableHeader}>SL No</th>
+                  <th className={styles.tableHeader}>Name</th>
+                  <th className={styles.tableHeader}>Price</th>
+                  <th className={styles.tableHeader}>Return</th>
+                </tr>
+              </thead>
+              <tbody className={styles.tableBody}>
+                <tr className={styles.tableRow}>
+                  <td className={styles.tableCell}>01</td>
+                  <td className={styles.tableCell}>Trivago</td>
+                  <td className={styles.tableCell}>$520</td>
+                  <td className={styles.tableCell}>+5%</td>
+                </tr>
+                <tr className={styles.tableRow}>
+                  <td className={styles.tableCell}>02</td>
+                  <td className={styles.tableCell}>Canon</td>
+                  <td className={styles.tableCell}>$480</td>
+                  <td className={styles.tableCell}>+10%</td>
+                </tr>
+                <tr className={styles.tableRow}>
+                  <td className={styles.tableCell}>03</td>
+                  <td className={styles.tableCell}>Uber Food</td>
+                  <td className={styles.tableCell}>$350</td>
+                  <td className={styles.tableCell}>-3%</td>
+                </tr>
+                <tr className={styles.tableRow}>
+                  <td className={styles.tableCell}>04</td>
+                  <td className={styles.tableCell}>Nokia</td>
+                  <td className={styles.tableCell}>$940</td>
+                  <td className={styles.tableCell}>+2%</td>
+                </tr>
+                <tr className={styles.tableRow}>
+                  <td className={styles.tableCell}>05</td>
+                  <td className={styles.tableCell}>Tiktok</td>
+                  <td className={styles.tableCell}>$670</td>
+                  <td className={styles.tableCell}>-12%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
