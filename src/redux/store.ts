c75a80b8a118834fea16fc71bc68e25fa5af2829
Amir <amir.khalidov@gmail.com>
2025-07-31 21:26:25 +0400

    Fix formatting issues in db.json and add missing imports in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,6 +4,9 @@ import transactionsReducer from "./transactionsSlice";
 import settingsReducer from "./settingsSlice";
 import loansReducer from "../redux/loanSlice";
 import servicesReducer from "../redux/servicesSlice";
+import investmentsReducer from "../redux/investmentsSlice";
+import trendingReducer from "../redux/trendingSlice";
+import uiReducer from "../redux/uiSlice";
 
 export const store = configureStore({
   reducer: {
@@ -12,8 +15,8 @@ export const store = configureStore({
     settings: settingsReducer,
     loans: loansReducer,
     services: servicesReducer,
+    investments: investmentsReducer,
+    trending: trendingReducer,
+    ui: uiReducer,
   },
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
